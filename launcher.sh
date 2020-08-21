84e1ca72a0668c9ea4cdb84ccd4ac982988d8a59
sat3ll <37741639+sat3ll@users.noreply.github.com>
2020-08-21 01:34:22 +0100

    Check permissions for thcrap POSIX compliance

--- launcher.sh
+++ launcher.sh
@@ -23,6 +23,12 @@ if [ ! -f "$XDG_DATA_HOME"/.thcrap-installed ]; then
   cp "$XDG_DATA_HOME"/thcrap/bin/*.dll "$XDG_DATA_HOME"/thcrap/
 fi
 
+check_permissions() {
+  if [ -z "$(grep "features.*devel" /.flatpak-info)" ] || [ -z "$(grep "shared.*network" /.flatpak-info)" ]; then
+    zenity --error --text "Required permissions missing. Consult README.md for details"
+    exit 1
+  fi
+}
 
 
 if [ -z "$1" ]; then
@@ -31,7 +37,9 @@ if [ -z "$1" ]; then
   # needed for sound and config to work
   cd /app/th07
   wine explorer /desktop=PCB,1920x1080 th07.exe
-elif [[ "$1" == "thcrap-conf" ]]; then
+elif [ "$1" = "thcrap-conf" ]; then
+
+  check_permissions
 
   # FIXME: workaround broken updater dll
   if [ ! -f "$XDG_DATA_HOME"/thcrap/thcrap_update.dll ]; then
@@ -39,7 +47,9 @@ elif [[ "$1" == "thcrap-conf" ]]; then
   fi
 
   wine "$XDG_DATA_HOME"/thcrap/thcrap_configure.exe
-elif [[ "$1" == "config-select" ]]; then
+elif [ "$1" = "config-select" ]; then
+
+  check_permissions
 
   # FIXME: workaround broken updater dll
   if [ -f "$XDG_DATA_HOME"/thcrap/thcrap_update.dll ]; then
@@ -50,7 +60,7 @@ elif [[ "$1" == "config-select" ]]; then
   if [ -z "$CONFS" ]; then
     zenity --error --text "No config files found! Please generate them first then try again"
   else
-    CHOICE=$(echo -n "$CONFS" | zenity --list --column "Available configs")
+    CHOICE=$(echo "$CONFS" | zenity --list --column "Available configs")
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
       wine explorer /desktop=PCB,1920x1080 "$XDG_DATA_HOME"/thcrap/thcrap_loader.exe "$CHOICE" "Z:\app\th07\th07.exe"
